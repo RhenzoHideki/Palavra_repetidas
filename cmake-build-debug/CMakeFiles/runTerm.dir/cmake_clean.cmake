fbb3ddeb57973541d90d3e4f602d1fce99498415
RhenzoHideki <rhskgames@gmail.com>
2020-03-10 11:32:45 -0300

    Primeiro commit

---
+++ cmake-build-debug/CMakeFiles/runTerm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runTerm.dir/Terminal.cpp.o"
+  "CMakeFiles/runTerm.dir/termtest.cpp.o"
+  "runTerm"
+  "runTerm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runTerm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
 // implemente aqui seu programa ...
+
+#include <iostream>
+#include <list>
+#include <string>
+#include <fstream>
+
+#include "Terminal.h"
+
+using namespace std;
+
+int main (int arc , char *argv[]){
+    string str =argv[1];
+    list<string> l;
+    string line;
+
+    ifstream arq(str);
+
+    if (!arq.is_open()) cout << "Não possivel abrir"<< endl;
+
+    while (getline(arq,line)) {
+        for (auto & compara: l) {
+            if (compara == line) cout << endl;
+            }
+        l.push_back(line);
+        cout << line << endl;
+    }
+    return 0;
+}
